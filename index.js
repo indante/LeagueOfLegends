f6d2ac6b51e2390ca7ef8eb1f97f60223ce5c56f
iam-dante <koreanhoseon@gmail.com>
2021-04-10 18:13:38 +0900

    code refactoring

--- index.js
+++ index.js
@@ -5,60 +5,52 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 const port = process.env.PORT
 
-const getHtml = async () => {
-    try {
-        return await axios.get("https://www.op.gg/summoner/spectator/userName=Begln%20again&", {
-            headers: {
-                'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36',
-                'accept-language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7'
-            }
-        })
-    } catch (error) {
-        console.log(error);
-    }
+function getHtml(username) {
+  try {
+    return axios.get(encodeURI(`https://www.op.gg/summoner/spectator/userName=${username}`), {
+      headers: {
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36',
+        'accept-language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7'
+      }
+    })
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-getHtml()
+function active() {
+  getHtml("White stardust")
     .then(html => {
-        const $ = cheerio.load(html.data);
-        const selector = $("div.tabItem.Content.SummonerLayoutContent.summonerLayout-spectator")
-
-        const text = $(selector).find("div > p:nth-child(2)").text()
-        console.log(text)
+      const $ = cheerio.load(html.data);
+      const activeInGame = $("div.SpectateSummoner > div > div.Content > table.Table.Team-100 > tbody > tr > td > a").text()
+      const timeCount = $("div.SpectateSummoner > div > div.Title > small.Time").text()
+      if (activeInGame) {
+        console.log(timeCount)
+      } else {
+        deactive()
+      }
     })
+};
 
+function deactive() {
+  getHtml("Begln again")
+    .then(html => {
+      const $ = cheerio.load(html.data);
+      const deactiveInGame = $("div.tabItem.Content.SummonerLayoutContent.summonerLayout-spectator > div > h2").text()
+      console.log(deactiveInGame)
+    })
+}
 
-// playwright
-// async function main(username) {
-    // const browser = await playwright.chromium.launch({
-    //     headless: false
-    // });
+active()
 
-    // const page = await browser.newPage();
+// getHtml("IIIllIlIllIIlIII")
+//     .then(html => {
+//         const $ = cheerio.load(html.data);
 
-    // const results = await page.goto(`https://www.op.gg/summoner/userName=${username}`);
+//         const deactiveInGame = $("div.tabItem.Content.SummonerLayoutContent.summonerLayout-spectator")
+//         const activeInGame = $("div.SpectateSummoner > div > div.Content > table.Table.Team-100 > tbody")
 
-//     const clickInGameButton = await page.click('dd.Item.tabHeader.inGame > a')
-//     const awaitSelector = await page.waitForSelector('#SummonerLayoutContent > div.tabItem.Content.SummonerLayoutContent.summonerLayout-spectator > div')
-//     const getInGameInfo = await page.$('#SummonerLayoutContent > div.tabItem.Content.SummonerLayoutContent.summonerLayout-spectator > div > p:nth-child(2)')
-//     const inGameDeactive = await page.$('#SummonerLayoutContent > div.tabItem.Content.SummonerLayoutContent.summonerLayout-spectator > div > h2')
-//     const test = await getInGameInfo.textContent()
-//     const test2 = await inGameDeactive.textContent()
-
-//     if (test == '현재 게임중이라면 다시 시도해주세요.'){
-//         console.log(test2)
-//         await browser.close();
-//     }
-// }
-
-// main("Begln again")
-
-
-// express
-// app.get('/', async (req,res) => {
-//     await res.send(main("Begln again"))
-// })
-
-// app.listen(port, () => {
-//     console.log(`Example app listening at http://localhost:${port}`)
-// })
+//         const deactiveText = $(deactiveInGame).find("div > p:nth-child(2)").text()
+//         const activeText = $(activeInGame).find("tr > td > a").text()
+//         console.log(activeText)
+    // })
