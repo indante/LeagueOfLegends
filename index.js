9f13f8bdbaf0048654d6a7f53c796e96e590e8e5
iam-dante <koreanhoseon@gmail.com>
2021-04-13 02:11:03 +0900

    lastly commit

--- index.js
+++ index.js
@@ -1,77 +1,49 @@
-require('dotenv').config()
-const express = require('express');
-const app = express();
-const axios = require('axios');
-const cheerio = require('cheerio');
-const port = process.env.PORT
-
-
-// checkIfUserIsInGame
-// input: userName (string)
-// output: Promise<boolean>
-// checkElapsedSecondsOfUser
-// input: userName (string)
-// output: secondsElapsed (Promise<number | null>)
-
-
-// function getHtml(username) {
-//   try {
-//     return axios.get(encodeURI(`https://www.op.gg/summoner/spectator/userName=${username}`), {
-//       headers: {
-        // 'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36',
-        // 'accept-language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7'
-//       }
-//     })
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
-
-// function checkIfUserIsInGame(username) {
-//   getHtml(username)
-//     .then(html => {
-//       const $ = cheerio.load(html.data);
-//       const deactiveInGame = $("div.tabItem.Content.SummonerLayoutContent.summonerLayout-spectator > div > h2").text()
-//       console.log(deactiveInGame)
-//     })
-// };
-
-// checkIfUserIsInGame("Begln again")
+const axios = require("axios");
+const cheerio = require("cheerio");
 
 
 function getHtml(username) {
-  return new Promise(function(resolve, reject) {
-    const test = axios.get(encodeURI(`https://www.op.gg/summoner/spectator/userName=${username}`), {
-      headers: {
-        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36',
-        'accept-language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7'
+  return axios
+    .get(
+      encodeURI(`https://www.op.gg/summoner/spectator/userName=${username}`),
+      {
+        headers: {
+          "user-agent":
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36",
+          "accept-language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
+        },
       }
-    })
-    resolve(test)
-  })
+    )
+    .then((res) => res.data);
 }
 
-getHtml
-// function active() {
-//   getHtml("가나라마아바")
-//     .then(html => {
-//       const $ = cheerio.load(html.data);
-//       const activeInGame = $("div.SpectateSummoner > div > div.Content > table.Table.Team-100 > tbody > tr > td > a").text()
-//       const timeCount = $("div.SpectateSummoner > div > div.Title > small.Time").text()
 
-//       if (activeInGame) {
-//         console.log(timeCount)
-//       } else {
-//         deactive()
-//       }
-//     })
-// };
+async function checkIfUserIsInGame(username) {
+  const html = await getHtml(username);
+  return html.includes("게임중이 아닙니다.") === false;
+}
 
-// function deactive() {
-//   getHtml("Begln again")
-//     .then(html => {
-//       const $ = cheerio.load(html.data);
-//       const deactiveInGame = $("div.tabItem.Content.SummonerLayoutContent.summonerLayout-spectator > div > h2").text()
-//       console.log(deactiveInGame)
-//     })
-// }
+
+async function checkElapsedSecondsOfUser(username) {
+  const html = await getHtml(username);
+
+  if (!checkIfUserIsInGame(username)) {
+    return null;
+  }
+
+  const $ = cheerio.load(html);
+  const opggElapsedTime = $(
+    "div.SpectateSummoner > div > div.Title > small.Time"
+  ).text();
+
+  return opggElapsedTime;
+}
+
+
+async function main() {
+  const result = await checkElapsedSecondsOfUser("라쿤99");
+  console.log(result);
+}
+
+checkIfUserIsInGame("Begln again")
+  .then(res => {console.log(res)})
